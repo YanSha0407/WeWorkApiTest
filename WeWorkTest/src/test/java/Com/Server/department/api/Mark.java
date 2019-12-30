39c3ffd899d1aa4ec912bf908c193fd702d75ee1
yansha <1224894560@qq.com>
2019-12-30 12:41:32 +0800

    添加注释

--- WeWorkTest/src/test/java/Com/Server/department/api/Mark.java
+++ WeWorkTest/src/test/java/Com/Server/department/api/Mark.java
@@ -3,6 +3,7 @@ package Com.Server.department.api;
 import Com.Server.Base.BaseWork;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,11 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Mark {
 
+    /**
+     * 获取标签列表
+     *
+     * @return
+     */
     public Response list() {
         return given().param("access_token", BaseWork.getInstance().getToken())
                 .when().log().all()
@@ -20,9 +26,17 @@ public class Mark {
                 .body("errcode", equalTo(0)).extract().response();
     }
 
+    /**
+     * 创建标签
+     *
+     * @param name
+     * @return
+     */
     public Response create(String name) {
-        // TODO :为什么 access_token 不可以传到body里 和参数一起掺入后台。
-        //  传到body 会提示 缺少access_token。
+        // TODO :为什么 access_token 不可以传到body里 和参数一起传给后台。添加到body 会提示 缺少access_token。
+        //  查看接口access_token是以key——value形式拼接在地址栏后
+        //  所以不能放在body体中 需用get请求传参的方式传入。
+
         System.out.println("getToken :  " + BaseWork.getInstance().getToken());
         HashMap<String, Object> map = new HashMap<>();
         map.put("tagname", name);
@@ -36,6 +50,12 @@ public class Mark {
                 .body("errcode", equalTo(0)).extract().response();
     }
 
+    /**
+     * 删除标签
+     *
+     * @param id
+     * @return
+     */
     public Response delete(int id) {
         System.out.println("-----id:  " + id);
         return given().param("access_token", BaseWork.getInstance().getToken())
@@ -48,6 +68,10 @@ public class Mark {
 
     /**
      * 更新标签名字
+     *
+     * @param id
+     * @param name
+     * @return
      */
     public Response updateMarkTitle(int id, String name) {
         Map<String, Object> map = new HashMap<>();
@@ -65,6 +89,9 @@ public class Mark {
 
     /**
      * 获取标签成员
+     *
+     * @param id
+     * @return
      */
     public Response getMember(int id) {
         return given().param("access_token", BaseWork.getInstance().getToken())
@@ -79,6 +106,10 @@ public class Mark {
 
     /**
      * 增加标签成员
+     *
+     * @param id
+     * @param userList
+     * @return
      */
     public Response addMember(int id, ArrayList<String> userList) {
         Map<String, Object> map = new HashMap<>();
@@ -89,8 +120,12 @@ public class Mark {
 
     /**
      * 删除标签成员
+     *
+     * @param id
+     * @param userList
+     * @return
      */
-    public Response deleteMember(int id, ArrayList<String > userList) {
+    public Response deleteMember(int id, ArrayList<String> userList) {
         Map<String, Object> map = new HashMap<>();
         map.put("tagid", id);
         map.put("userlist", userList);
